3d67d9bd0343d1c7a84925f6ee2deb34d4bd2153
imakero <45946863+imakero@users.noreply.github.com>
2021-11-20 15:58:13 +0100

    feature: add all events page

--- pages/events.js
+++ pages/events.js
@@ -1,9 +1,10 @@
 import Event from '../components/Event'
 import { sanityClient } from '../sanity'
 
-export default function Home({ events }) {
+export default function Events({ events }) {
   return (
-    <main className="home">
+    <main>
+      <h1>All Events</h1>
       <section className="event-grid">
         {events.map((event) => (
           <Event event={event} key={event._id} />
@@ -14,7 +15,7 @@ export default function Home({ events }) {
 }
 
 export const getStaticProps = async () => {
-  const query = '*[ _type == "event" && featured]'
+  const query = '*[ _type == "event"]'
   const events = await sanityClient.fetch(query)
 
   return {
