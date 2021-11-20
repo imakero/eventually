845f9324a0b56729bf875f8bfd3b14906d46dad8
imakero <45946863+imakero@users.noreply.github.com>
2021-11-20 15:46:27 +0100

    fix: add sanity api version

--- sanity.js
+++ sanity.js
@@ -3,6 +3,7 @@ import { createClient, createImageUrlBuilder } from 'next-sanity'
 const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-10-21',
   useCdn: process.env.NODE_ENV === 'production',
 }
 
