0cbca10ae6a4c7b4b7b23a0add414b779b5a17a1
imakero <45946863+imakero@users.noreply.github.com>
2021-11-23 11:17:35 +0100

    style admin page

--- pages/admin.js
+++ pages/admin.js
@@ -41,84 +41,76 @@ export default function Admin() {
   //console.log(title, date, description, agenda, category)
 
   return (
-    <main>
-      <img src={img} />
+    <main className="page-admin">
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            title="title"
-            id="title"
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            name="date"
-            id="date"
-            value={date}
-            onChange={(event) => setDate(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="description">Short description</label>
-          <textarea
-            name="description"
-            id="description"
-            cols="30"
-            rows="10"
-            value={description}
-            onChange={(event) => setDescription(event.target.value)}
-          ></textarea>
-        </div>
-        <div>
-          <label htmlFor="agenda">Agenda</label>
-          <textarea
-            name="agenda"
-            id="agenda"
-            cols="30"
-            rows="10"
-            value={agenda}
-            onChange={(event) => setAgenda(event.target.value)}
-          ></textarea>
-        </div>
-        <div>
-          <label htmlFor="cover-image">Cover image</label>
-          <input
-            type="file"
-            name="cover-image"
-            id="cover-image"
-            accept="image/*"
-            onChange={(event) => setCoverImage(event.target.files[0])}
-          />
-        </div>
-        <div>
-          <label htmlFor="images">Event images</label>
-          <input
-            type="file"
-            name="images"
-            id="images"
-            accept="image/*"
-            multiple
-          />
-        </div>
-        <div>
-          <label htmlFor="category"></label>
-          <select
-            name="category"
-            id="category"
-            value={category}
-            onChange={(event) => setCategory(event.target.value)}
-          >
-            <option value="entertainment">Entertainment</option>
-            <option value="after-work">After work</option>
-            <option value="education">Education</option>
-          </select>
-        </div>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          title="title"
+          id="title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+
+        <label htmlFor="date">Date</label>
+        <input
+          type="date"
+          name="date"
+          id="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
+
+        <label htmlFor="description">Short description</label>
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        ></textarea>
+
+        <label htmlFor="agenda">Agenda</label>
+        <textarea
+          name="agenda"
+          id="agenda"
+          cols="30"
+          rows="10"
+          value={agenda}
+          onChange={(event) => setAgenda(event.target.value)}
+        ></textarea>
+
+        <label htmlFor="cover-image">Cover image</label>
+        <input
+          type="file"
+          name="cover-image"
+          id="cover-image"
+          accept="image/*"
+          onChange={(event) => setCoverImage(event.target.files[0])}
+        />
+
+        <label htmlFor="images">Event images</label>
+        <input
+          type="file"
+          name="images"
+          id="images"
+          accept="image/*"
+          multiple
+        />
+
+        <label htmlFor="category"></label>
+        <select
+          name="category"
+          id="category"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="entertainment">Entertainment</option>
+          <option value="after-work">After work</option>
+          <option value="education">Education</option>
+        </select>
+
         <button type="submit">Skapa event</button>
       </form>
     </main>
